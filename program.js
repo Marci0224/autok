80065a48e44bf4d0156d9de868642267ae68ebab
Marci0224 <losonczim0224@gmail.com>
2026-01-30 11:29:44 +0100

    update program.js local

--- program.js
+++ program.js
@@ -1,8 +1,8 @@
 import { legjobb } from "./autok.js";
 
-console.log("Program elindult");
+console.log("A program elindult");
 
-for (let i=0; i<=7; i++) {
+for (let i=1; i<=6; i++) {
     console.log(i+".", "autó");
 }
 legjobb();
